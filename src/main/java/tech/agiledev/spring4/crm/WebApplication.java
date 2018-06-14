b58e8c948f1950e71e2c0070a82be7f7d1ddff82
ericsiber <eric.siber@gmail.com>
2018-06-14 11:08:30 +0200

    compléments

--- src/main/java/tech/agiledev/spring4/crm/WebApplication.java
+++ src/main/java/tech/agiledev/spring4/crm/WebApplication.java
@@ -2,18 +2,23 @@ package tech.agiledev.spring4.crm;
 
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.webapp.WebAppContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class WebApplication {
+	private static Logger LOGGER = LoggerFactory.getLogger(WebApplication.class);
+
 	public static void main(String[] args) throws Exception {
-		Server server = new Server(80);
+		Server server = new Server(8080);
 
 		WebAppContext webAppContext = new WebAppContext();
 		webAppContext.setDescriptor("src/main/webapp/WEB-INF/web.xml");
 		webAppContext.setResourceBase("src/main/webapp/");
-		webAppContext.setContextPath("/servlet-rest-service");
+		webAppContext.setContextPath("/crm");
 		server.setHandler(webAppContext);
 
 		server.start();
+		LOGGER.debug("Web application ready");
 		server.join();
 	}
 }

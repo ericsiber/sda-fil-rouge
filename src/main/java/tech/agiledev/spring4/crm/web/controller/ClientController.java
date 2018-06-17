a18121d032c3264f4f6a4d148093736b1b862422
ericsiber <eric.siber@gmail.com>
2018-06-17 17:12:39 +0200

    Avec un @RequestParam

--- src/main/java/tech/agiledev/spring4/crm/web/controller/ClientController.java
+++ src/main/java/tech/agiledev/spring4/crm/web/controller/ClientController.java
@@ -6,8 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.View;
-import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import tech.agiledev.spring4.crm.modele.Client;
 import tech.agiledev.spring4.crm.service.ClientService;
@@ -23,9 +22,8 @@ public class ClientController {
 	}
 
 	@RequestMapping(value = "/client", method = RequestMethod.GET)
-	public String afficherClient(HttpServletRequest req) {
-		String idStr = req.getParameter("id");
-		Client client = clientService.findById(new Long(idStr));
+	public String afficherClient(@RequestParam("id") Long idClient, HttpServletRequest req) {
+		Client client = clientService.findById(idClient);
 		req.setAttribute("client", client);
 		return "client";
 	}
